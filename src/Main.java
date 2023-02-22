a380dd8ba97628086df3c6749c3ee5e8f4796534
Kirito Moreno <khirann.moreno96@gmail.com>
2023-02-22 19:09:45 +0000

    Método constructor cla.6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Doctor myDoctor = new Doctor("Andres Torres");
+        myDoctor.name = "Alejo Moreno";
+        myDoctor.showName();
     }
 }
