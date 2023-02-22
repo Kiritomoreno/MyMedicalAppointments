4523afcb8be7e77274a338aecbe84afa953eeb87
Kirito Moreno <khirann.moreno96@gmail.com>
2023-02-22 19:29:32 +0000

    Creando elementos estáticos clas.8

---
+++ src/Doctor.java
@@ -0,0 +1,25 @@
+import javax.print.Doc;
+
+public class Doctor {
+    static int id= 0; // Autoincrement
+    String name;
+    String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
+
+    //Comportamientos
+    // Nuestro primer metodo
+    public void showName(){
+        System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.util.function.DoubleToIntFunction;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Llevamos el control del autoincremento con elementos estáticos
         Doctor myDoctor = new Doctor("Andres Torres");
         myDoctor.name = "Alejo Moreno";
         myDoctor.showName();
+        myDoctor.showId();
+
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        UIMenu.showMenu();
+
     }
+
+
 }
