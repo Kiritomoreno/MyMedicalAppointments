7e3b1d372e1465833d392f68445c125024834d3a
Kirito Moreno <khirann.moreno96@gmail.com>
2023-02-23 15:57:48 +0000

    Creación de la case Patient class.12

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    //Atributos
+    int id;
+    String name;
+    String email;
+    String address;
+    String birthday;
+    double weight;
+    double height;
+    String blood;
+
+// Método constructor sobre cargado
+    Patient(String name, String email){
+        this.name = name;
+        this.email= email;
+    }
+}
